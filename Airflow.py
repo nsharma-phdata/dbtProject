fb68058c318f0a22c3596ac611a52bc44371edf5
nsharma <nsharma@phdata.io>
2022-08-26 09:10:17 +0530

    airflow_dag

---
+++ Airflow.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import datetime
+from airflow.utils.dates import timedelta
+
+with DAG(
+    dag_id='dbt_dag',
+    start_date=datetime(2022, 08, 17),                        
+    description='My Airflow DAG to invoke dbt commands',
+    schedule_interval=timedelta(days=1),
+) as dag:
+
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command='dbt run'
+    )
+
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command='dbt test'
+    )
+
+    dbt_run >> dbt_test
+
+    
